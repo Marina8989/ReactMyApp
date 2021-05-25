7ccc6592af4caf1e92289295c043acebc056436b
Marina8989 <marina.mill209@gmail.com>
2021-05-25 16:46:00 -0700

    react course

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 
-export default function App() {
-
-     return <div>Hello React</div>;
+class App extends React.Component {
+     constructor(props) {
+          super(props)
+           this.state = {title: 'hello react'}
+     }
+   render(){
+      return <div>{this.state.title}</div>;
+   }
 }
  
+export default App

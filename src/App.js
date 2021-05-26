1536a4667044e74fd38dcde00508898758acb5ed
Marina8989 <marina.mill209@gmail.com>
2021-05-25 17:14:54 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 
-class App extends React.Component {
-     constructor(props) {
-          super(props)
-           this.state = {title: 'hello react'}
-     }
-   render(){
-      return <div>{this.state.title}</div>;
-   }
+function App( {title}) {
+     return (
+       <div>
+          <div style={{padding: '2rem', 
+          border: '1px solid green', 
+          backgroundColor: 'grey', 
+          color: 'white'}}
+          >{title}</div>
+       </div>
+     ) 
 }
+
+
+
+
+
+// class App extends React.Component {
+//      constructor(props) {
+//           super(props)
+//            this.state = {title: 'hello react'}
+//      }
+//    render(){
+//       return <div>{this.state.title}</div>;
+//    }
+// }
  
 export default App
